ed5f1c74775851ad30d0367f4528ba5ce488ad34
Guendouz mehdi <mahdign12@gmail.com>
2023-03-10 20:23:58 +0100

    about page

---
+++ src/app/about/page.js
@@ -0,0 +1,27 @@
+
+
+const About = () => {
+    return (
+        <div className="max-w-6xl mx-auto space-y-4">
+            <h1 className="font-medium text-amber-600 text-2xl ">About</h1>
+            <p className="">
+                Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been 
+                 standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 
+                 to make a type specimen book. It has survived not only five centuries, but also the leap into electronic 
+                 typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset 
+                 sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like
+                 Aldus PageMaker including versions of Lorem Ipsum.
+            </p>
+            <p>
+                Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been 
+                 standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 
+                 to make a type specimen book. It has survived not only five centuries, but also the leap into electronic 
+                 typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset 
+                 sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like
+                 Aldus PageMaker including versions of Lorem Ipsum.
+            </p>
+        </div>
+    );
+}
+
+export default About;
