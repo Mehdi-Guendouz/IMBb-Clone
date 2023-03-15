b7652028084553f33afd82398c1af407c683523a
Guendouz mehdi <mahdign12@gmail.com>
2023-03-15 11:33:51 +0100

    single movie fetch

--- src/component/Card.jsx
+++ src/component/Card.jsx
@@ -6,7 +6,7 @@ import { FiThumbsUp } from 'react-icons/fi';
 const Card = ({result}) => {
     return (
         <div className='cursor-pointer sm:p-3 sm:hover:shadow-slate-400 sm:hover:shadow-md rounded-lg sm:border sm:border-slate-400 sm:m-2 transition-shadow duration-200 group'>
-            <Link href={`movies/${result.id}`} >
+            <Link href={`movie/${result.id}`} >
                 <Image 
                     src={`https://image.tmdb.org/t/p/original/${result.backdrop_path || result.poster_path}`} 
                     alt={"movie img"} 
