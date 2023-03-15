234324d81e8dcc442eb2f46bd109d1baa82bf348
Guendouz mehdi <mahdign12@gmail.com>
2023-03-15 12:43:03 +0100

    update icon & last commit

--- src/component/Header.jsx
+++ src/component/Header.jsx
@@ -15,7 +15,7 @@ const Header = () => {
                 <DarkModeSwitch />
                 <Link href={"/"}>
                     <h2 className='text-2xl'>
-                        <span className='font-bold bg-amber-500 px-2 py-1 rounded-lg mr-1'>IMDb</span>
+                        <span className='font-bold bg-amber-500 px-2 py-1 rounded-lg mr-1'>IMDG</span>
                         <span className='text-xl hidden sm:inline'>Clone</span>
                     </h2>
                 </Link>
